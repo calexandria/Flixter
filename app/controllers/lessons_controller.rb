7b3b054bd1efa04c0bee3b2086e8fadd7ebba3d1
Alexandria <cailey92@yahoo.com>
2019-09-17 18:57:22 +0000

    adding stripe payment capabilities

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+before_action :require_authorized_for_current_lesson
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path(@course), alert: 'You are not enrolled!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
